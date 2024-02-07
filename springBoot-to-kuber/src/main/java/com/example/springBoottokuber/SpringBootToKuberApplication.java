8ec10b0ad877865b7d6a98b96d63695641c09b78
kurgashov_aa <aleksandr.kurgashov@open.ru>
2024-02-07 14:38:10 +0300

    Add SpringBoot Module

---
+++ springBoot-to-kuber/src/main/java/com/example/springBoottokuber/SpringBootToKuberApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBoottokuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToKuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootToKuberApplication.class, args);
+	}
+
+}
